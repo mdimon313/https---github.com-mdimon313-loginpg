6bc33d6b40f86bae6a469acba97555781b125660
mdimon313 <imon26362@gmail.com>
2024-03-02 13:47:47 +0600

    demo porpuse

---
+++ assets/script/main.js
@@ -0,0 +1,33 @@
+const form = document.querySelector("#loginForm"),
+loginBtn = document.querySelector("#loginBtn"),
+chkBox = document.querySelector("#chkbox");
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const obj = {
+        username: e.target[0].value,
+        password: e.target[0].value
+    }
+    console.log(obj);
+
+    if(chkBox){
+        // coocke.setItem("logon", JSON.stringify(obj));
+        
+        // let val = document.getElementById("cookie-value");
+    let uName = JSON.parse(document.cookie).username;
+    let psss = JSON.parse(document.cookie).password;
+    if(e.target[0].value == uName)
+        console.log('same')
+    } else {
+        document.cookie = `${JSON.stringify(obj)}; SameSite=Lax;`
+        console.log(chkBox)
+    }
+})
+
+window.addEventListener("online", ()=>{
+    console.log("online");
+})
+
+window.addEventListener("offline",()=>{
+    document.querySelector("body").innerHTML ="Your are currently offline";
+})
